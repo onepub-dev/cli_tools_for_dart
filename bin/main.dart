66d3e3e304d845313771ab2ff522625bfb2ce199
bsutton <bsutton@noojee.com.au>
2019-10-10 11:18:30 +1100

    first code push

---
+++ bin/main.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:args/args.dart';
+import 'package:path/path.dart' as p;
+
+void main(List<String> arguments) {
+  ArgParser parser = ArgParser();
+  ArgResults results = parser.parse(arguments);
+  // parser.addOption('files', abbr: 'f');
+  // parser.addCommand("import");
+
+  if (results.rest.length != 2) {
+    usage(parser);
+    exit(-1);
+  }
+  String fromPattern = results.rest[0];
+  String toPattern = results.rest[1];
+
+  process(fromPattern, toPattern);
+}
+
+void process(String fromPattern, String toPattern) async {
+  Directory cwd = Directory(".");
+
+  Stream<FileSystemEntity> files = cwd.list(recursive: true);
+
+  List<FileSystemEntity> dartFiles =
+      await files.where((file) => file.path.endsWith(".dart")).toList();
+
+  for (var file in dartFiles) {
+    replaceString(file, fromPattern, toPattern);
+  }
+}
+
+void replaceString(
+    FileSystemEntity file, String fromPattern, String toPattern) async {
+  Directory systemTempDir = Directory.systemTemp;
+
+  String tmpPath = p.join(systemTempDir.path, file.path);
+  File tmpFile = File(tmpPath);
+
+  Directory tmpDir = Directory(tmpFile.parent.path);
+  await tmpDir.create(recursive: true);
+
+  IOSink tmpSink = tmpFile.openWrite();
+
+  await File(file.path)
+      .openRead()
+      .transform(utf8.decoder)
+      .transform(LineSplitter())
+      .forEach((line) => replaceLine(line, fromPattern, toPattern, tmpSink));
+
+  print("File: ${file.path} written to: ${tmpFile}");
+}
+
+void replaceLine(
+    String line, String fromPattern, String toPattern, IOSink tmpSink) {
+  String newLine = line.replaceAll(fromPattern, toPattern);
+  tmpSink.writeln(newLine);
+}
+
+void usage(ArgParser parser) {
+  print("Usage: ");
+  print("<from string> <to string>");
+  print("e.g. AppClass app_class");
+  print(parser.usage);
+}
---
+++ test/square_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:square_cli/square_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
