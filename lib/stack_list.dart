9aacc150d92b9826d08110250bf72e5855e186fb
bsutton <bsutton@noojee.com.au>
2019-10-11 11:26:50 +1100

    new stacktrace.

---
+++ lib/stack_list.dart
@@ -0,0 +1,32 @@
+import 'dart:collection';
+
+///
+/// A classic Stack of items with a push and pop method.
+///
+class StackList<T> {
+  Queue<T> stack = Queue();
+
+  ///
+  /// Creates a stack from [initialStack]
+  /// by pushing each element of the list
+  /// onto the stack from first to last.
+  StackList.fromList(List<T> initialStack) {
+    for (T item in initialStack) {
+      push(item);
+    }
+  }
+
+  void push(T item) {
+    stack.addFirst(item);
+  }
+
+  T pop() {
+    return stack.removeFirst();
+  }
+
+  /// returns the item onf the top of the stack
+  /// but does not remove the item.
+  T peek() {
+    return stack.first;
+  }
+}
